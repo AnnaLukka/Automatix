e1cc28beba001d65f767cce89276b637829250a4
AnnaLukka <snjatkova@gmail.com>
2023-07-04 22:11:02 +0300

    Выполнила дз

---
+++ src/main/java/HW3.java
@@ -0,0 +1,161 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HW3 {
+   /*1)*/
+    /*public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+        driver.findElement(By.xpath("//span[@class='userbar__button-text']")).click();
+*/
+        /*public static void main(String[] args) {
+            System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+        driver.findElement(By.xpath("//div[@class='userbar j-user-tabs']")).click();*/
+
+    /*public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+        driver.findElement(By.xpath("//div[@data-skin='header_top']")).click();*/
+
+   /* 2)*/
+   /*public static void main(String[] args) throws InterruptedException {
+       System.setProperty("webdriver.chrome.driver", "/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+       WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+       Thread.sleep(2000);
+       driver.findElement(By.xpath("//div[@class='footer__heading']/..//a[@href='/ru/sobaki/']")).click();
+       driver.quit();*/
+
+
+   /*3)*/
+   /*public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://dan-it.com.ua/uk/");
+        System.out.println(driver.findElement(By.xpath("//h1[contains(text(),'IT-освіта')]")).getText());
+       driver.findElement(By.xpath("//a[contains(text(),'Детальніше')]")).click();
+*/
+
+   /*4)*/
+     /*public static void main(String[] args) throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//a[@href='/ru/akvariumistika/1124/']/../../div[2]//span[contains(text(),'Аквариумы')]")).click();
+            driver.quit();*/
+
+   /*5)*/
+  /*public static void main(String[] args) {
+      System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+      WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://dan-it.com.ua/uk/");
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//header[@class='page__header page-header']"));
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//a[@href='tel:0 800 335 695']")).click();
+      driver.quit();*/
+
+   /*6)*/
+   /*public static void main(String[] args) throws InterruptedException {
+      System.setProperty("webdriver.chrome.driver", "/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+      WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://dan-it.com.ua/uk/");
+      Thread.sleep(2000);
+      System.out.println(
+              driver.findElement(By.xpath("//li[@class='banner-meta__item banner-meta__item--main banner-meta__item--main--offline']"))
+                      .getText());
+      driver.quit();*/
+
+      /*7)*/
+ /*public static void main(String[] args) throws InterruptedException {
+     System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+     WebDriver driver = new ChromeDriver();
+     driver.manage().window().maximize();
+     driver.get("https://www.w3schools.com/");
+     Thread.sleep(4000);
+     driver.findElement(By.id("accept-choices")).click();
+     driver.findElement(By.xpath("//a[@id = 'signupbtn_topnav']")).click();
+     driver.quit();*/
+
+ /*  public static void main(String[] args) throws InterruptedException {
+      System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+      WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://www.w3schools.com/");
+      Thread.sleep(4000);
+      driver.findElement(By.id("accept-choices")).click();
+      driver.findElement(By.xpath("//a[@id ='navbtn_services']")).click();
+      driver.quit();*/
+
+     /* public static void main(String[] args) throws InterruptedException {
+      System.setProperty("webdriver.chrome.driver","/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+      WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://www.w3schools.com/");
+      Thread.sleep(4000);
+      driver.findElement(By.id("accept-choices")).click();
+      driver.findElement(By.xpath("//a[@id = 'navbtn_tutorials']")).click();
+      driver.quit();*/
+
+      /*8)*/
+      /*public static void main (String[]args) throws InterruptedException {
+         System.setProperty("webdriver.chrome.driver", "/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+         WebDriver driver = new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.get("https://dan-it.com.ua/uk/");
+         Thread.sleep(2000);
+         System.out.println(
+                 driver.findElement(By.xpath
+                                 ("//li[@class='main-info__item'][1]/../..//child::li[@class='main-expertise__point'][3]"))
+                         .getText());
+         driver.quit();*/
+
+   /*9)*/
+
+    /*  public static void main (String[]args) throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver", "/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.jetbrains.com/idea/download/?section=mac");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//a[@class='wt-col-inline menu-second__download-button wt-button wt-button_size_s wt-button_mode_primary']")).click();
+            driver.quit();
+*/
+      /*  10)*/
+   /* public static void main (String[]args) throws InterruptedException {
+          System.setProperty("webdriver.chrome.driver", "/Users/ann_snj/Desktop/Chrome Driver/chromedriver");
+          WebDriver driver = new ChromeDriver();
+          driver.manage().window().maximize();
+          driver.get("https://www.jetbrains.com/idea/download/?section=mac");
+        Thread.sleep(2000);
+        driver.findElement(By.id("ez-accept-all")).click();
+          Thread.sleep(2000);
+          driver.findElement(By.xpath("//a[@href='/idea/download/']/../following::a[@data-test='dropdown-button-link'][1]")).click();
+          driver.quit();*/
+
+}
+
+
+
+
+
+
+
+
+
+
